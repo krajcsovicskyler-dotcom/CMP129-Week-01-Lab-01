9d3dc3aca29c49cceb7de6d2dc11ad1000f6e8f9
krajcsovicskyler-dotcom <krajcsovics.kyler@student.ccm.edu>
2026-09-01 16:47:28 -0400

    finished problem 1 working on problem 2

--- BoxOffice.java
+++ BoxOffice.java
@@ -1,4 +1,24 @@
-public class BoxOffice {
-    
-    
-}
+import java.util.Scanner;
+    public class BoxOffice {
+
+
+    public static void main (String[] args)
+    {
+        Scanner input =new Scanner(System.in);
+        int ATP = 10;
+        int CTP = 6;
+        int ATS = 0;
+        int CTS = 0;
+        String movie = " ";
+        System.out.println("What is the name of your movie?");
+        movie = input.nextLine();
+        System.out.println("How many adult tickets did you sell?");
+        ATS = input.nextInt();
+        System.out.println("How many Childrens tickets did you sell?");
+        CTS = input.nextInt();
+        int gross = ATS*ATP+CTS*CTP;
+        double net = gross*.2;
+        double distributor = gross-net;
+        System.out.println(" Movie name:     " + movie + "\n Adult tickets sold:     " + ATS + "\n Childrens tickets sold:     " + CTS + "\n Gross box-office profit:     " + gross + "\n Net theatre profit:     " + net + "\n Amount paid to the distributor:     " + distributor);
+    }//end main method
+}//end class
--- InterestEarned.java
+++ InterestEarned.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
 public class InterestEarned {
-    
-}
+
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        double p = 0;
+        double ir = 0;
+        double c = 0;
+        System.out.println("What is the principal amount?");
+        p = input.nextDouble();
+        System.out.println("What is the annual interest rate as a decimal?");
+        ir = input.nextDouble();
+        System.out.println("How many times is the interest compounded during the year?");
+        c = input.nextDouble();
+        double FA =p*Mth.pow(1+ir/c, c)
+        double IE = FA-p;
+    }//ends main
+}//ends class
